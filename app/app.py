d10a73218d5f5abda7b3044483e3836a65b173eb
JanhaviSingh2004 <janhavisinghchurk@gmail.com>
2026-09-14 01:44:54 +0530

    Connect application to MySQL

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +13,24 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "healthy"}
+
+
+@app.route("/db-health")
+def db_health():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST", "mysql"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+        )
+
+        connection.close()
+
+        return {"database": "connected"}
+
+    except Exception as e:
+        return {"database": "not connected", "error": str(e)}, 500
 
 
 if __name__ == "__main__":
